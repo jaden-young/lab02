c4c553643dd0af3b4accfe4800847f25b673446f
jaden.young <jaden.young@CL-QBB00116-41.ad.ndsu.edu>
2015-01-20 12:36:16 -0600

    Shtuff

--- src/lab02/Bag.java
+++ src/lab02/Bag.java
@@ -5,7 +5,12 @@ package lab02;
  */
 public interface Bag {
     
-    
-    
-    
+    public int getCurrentSize();
+    public boolean isEmpty();
+    public void add(int num);
+    public void remove(int n);
+    public void remove();
+    public void clear();
+    public int getFrequencyOf(int num);
+    public boolean contains(int num);
 }
